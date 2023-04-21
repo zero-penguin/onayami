81324577fbf62fdcfe252db1b7b8d20ac2a65517
zero-penguin <arato1060@outlook.jp>
2023-04-21 12:10:19 +0900

    setting.pyの変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'top',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
